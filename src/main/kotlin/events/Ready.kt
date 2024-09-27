a8cca6c2e5062f36f92d2a77dd14c7d3a9fb372e
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2024-09-27 17:49:03 +0200

    Update Ready.kt

--- src/main/kotlin/events/Ready.kt
+++ src/main/kotlin/events/Ready.kt
@@ -1,14 +1,20 @@
 package org.example.events
 
+import dev.kord.common.entity.PresenceStatus
 import dev.kord.core.Kord
 import dev.kord.core.event.gateway.ReadyEvent
 import dev.kord.core.on
+import org.example.BotConfig
 
 
 class BotReadyHandler : EventHandler {
     override suspend fun register(kord: Kord) {
         kord.on<ReadyEvent> {
             println("Logged in as ${self.tag} (${self.id})")
+            kord.editPresence {
+                status = PresenceStatus.Online
+                listening("music on all servers! | Type: ${BotConfig.discord.prefix}help")
+            }
         }
     }
 }
